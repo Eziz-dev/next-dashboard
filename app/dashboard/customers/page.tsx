aae0a975493655a488ded619e190d63319929805
Eziz-dev <ezka.tm97@gmail.com>
2025-02-07 00:23:43 +0500

    2.0.0

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,9 @@
+import { Metadata } from 'next';
+
+const metadata: Metadata = {
+  title: 'Customers',
+};
+
 export default function CustomersPage() {
-  return (
-    <p>Customers Page</p>
-  );
+  return <p>Customers Page</p>;
 }
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,13 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
- 
+import { Metadata } from 'next';
+
+const metadata: Metadata = {
+  title: 'Dashboard',
+  description: 'Dashboard',
+};
+
+export const experimental_ppr = true;
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
